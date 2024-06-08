d6b411a58ec30d4354a6bc495e9460e32a21958f
asishshaji <asishshajithomas@gmail.com>
2024-06-08 21:19:08 +0530

    deleted and added apps

--- install/set-git.sh
+++ install/set-git.sh
@@ -1,3 +1,6 @@
+git config --global user.email "asishshajithomas@gmail.com"
+git config --global user.name "asishshaji"
+
 # Set common git aliases
 git config --global alias.co checkout
 git config --global alias.br branch
